a4dbb4b20e5e55fe4879fb3251d040b96b09fff0
Bhansali, Mithul <mithul.bhansali@dieboldnixdorf.com>
2018-01-18 20:29:56 +0530

    2C - Import of script not working. Constants shifted. Status and Comment Determination must be better.Not created TestSetVos still working

--- script/JUnitXRayIntegration.ps1
+++ script/JUnitXRayIntegration.ps1
@@ -1,3 +1,209 @@
-. 'C:\Users\bhansm\Documents\My Received Files\powershell\XRayEntityVosDefinition.ps1'
+class JUnitXmlProcessor{
+    [System.Xml.XmlDocument]$file
+    [XrayTestEntityVo[]]$testVos = @()
+    [XrayTestSetEntityVo[]]$testSetVos = @()
+    [XrayTestPlanEntityVo]$testPlanVo
+    [XrayTestExecutionEntityVo]$testExecutionVo
 
-$vo = [XrayTestEntityVo]::getInstance()
+    [string]$startDate;
+    [string]$endDate;
+    [string]$suiteName;
+
+    JUnitXmlProcessor($filePath){
+        [System.Xml.XmlDocument]$this.file = new-object System.Xml.XmlDocument
+        $this.file.load($filePath)
+        $this.init()
+    }
+
+    init(){
+        $this.PopulateSuiteInfo()
+    }
+
+    PopulateSuiteInfo(){
+        $this.RootNodeHandler($this.file.SelectNodes("/testsuite"))
+    }
+
+    TestSuiteNodeHandler($suiteNode){
+        $this.suiteName = $suiteNode.testsuitename
+    }
+
+    RootNodeHandler($root_node){
+        $this.suiteName = $root_node.name
+    }
+
+    [XrayTestEntityVo] handleTestCaseNode($testCaseNode){
+        $testFields = [Fields]::new()
+        $testFields.description = $testCaseNode.classname + ":" + $testCaseNode.name
+        Write-Host "Desc: " $testFields.description
+        $testFields.summary = $testFields.description
+        $testFields.issuetype = [IssueType]::new("Test")
+        $testFields.project = [Project]::new([Constants]::projectKey)
+        $testFields.customfield_10400 = [TestType]::new("Generic")
+        $testFields.customfield_10403 = "Generic test definition"
+        $comment = $this.getComment($testCaseNode)
+        [XrayTestEntityVo]$testVo = [XrayTestEntityVo]::new($testFields)
+        $testVo.setStatus($this.getStatus($testCaseNode))
+        $testVo.setComment($comment)
+
+        return $testVo
+    }
+
+    [string] getStatus($test_case_node){
+          $comment = $null;
+          $hasChildNodes = $test_case_node.HasChildNodes;
+          
+          if($hasChildNodes) {
+            foreach($childNode in $test_case_node.ChildNodes){
+                $childNodeName = $childNode.LocalName
+                if($childNodeName -eq 'failure'){
+                    return 'FAIL'
+                } elseif ($childNodeName -eq 'error'){
+                    return 'FAIL'
+                } elseif ($childNodeName -eq 'skipped'){
+                    return 'TODO'
+                } else {
+                    Write-Host "Status need to be defined"
+                    return 'UNKNOWN'
+                }
+            }
+            return 'UNKNOWN'
+          } else {
+            return 'PASS'
+          }
+          
+    }
+    
+    [string] getComment($test_case_node){
+          $comment = $null;
+          $hasChildNodes = $test_case_node.HasChildNodes;
+
+          if($hasChildNodes) {
+            foreach($childNode in $test_case_node.ChildNodes){
+                $childNodeName = $childNode.LocalName
+                if($childNodeName -eq 'failure'){
+                    $comment = $childNode.InnerText
+                } elseif ($childNodeName -eq 'error'){
+                    $comment = $childNode.InnerText
+                } elseif ($childNodeName -eq 'skipped'){
+                    $comment = 'Skipped'
+                }
+            }
+          } else {
+            $comment = 'Execution Successful.'
+          }
+          Write-Host "Comment " $comment
+          return $comment
+    }
+
+    [XrayTestEntityVo[]] handleIterationContainerNode($iterationContainerNode){
+        Write-Host "Processing Iteration Container Node...."
+        [XrayTestEntityVo[]]$testArr = @()
+        [string]$activityType = "";
+        foreach ($childNodeOfIterationContainer in $iterationContainerNode.ChildNodes) {
+            $activityType = $childNodeOfIterationContainer.type
+            if($activityType -eq 'test-case'){
+                $testArr = $testArr + $this.handleTestCaseNode($childNodeOfIterationContainer)
+            }
+        }
+        Write-Host "Found: " + $testArr.Count
+        return $testArr
+    }
+
+    [XrayTestEntityVo[]] handleSmartFolderNode($smartFolderNode){
+        Write-Host "Processing Smart Folder Node...."
+        [XrayTestEntityVo[]]$testArr = @()
+        [string]$activityType = "";
+        foreach ($childNodeOfsmartFolder in $smartFolderNode.ChildNodes) {
+            $activityType = $childNodeOfsmartFolder.type
+            if($activityType -eq 'test-case'){
+                $testArr = $testArr + $this.handleTestCaseNode($childNodeOfsmartFolder)
+            } elseif ($activityType -eq 'iteration-container'){
+                $testArr = $testArr + $this.handleIterationContainerNode($childNodeOfsmartFolder)
+            } elseif ($activityType -eq 'smart-folder'){
+                $testArr = $testArr + $this.handleSmartFolderNode($childNodeOfsmartFolder)
+            }
+        }
+        Write-Host "Found: " $testArr.Count
+        return $testArr
+    }
+
+    CreateTestSetVos(){
+        $this.testSetVos = @() 
+        $smartFolderNodes= $this.file.SelectNodes("//activity[@type='test-suite']/activity[@type='smart-folder']")
+        [int]$count = 0
+        $activtyType = ''
+        foreach ($smartFolderNode in $smartFolderNodes) {
+            Write-Host "Processing Next SmartFolder"
+            $this.testSetVos = $this.testSetVos + [XrayTestSetEntityVo]::new($this.handleSmartFolderNode($smartFolderNode))
+            Write-Host "Finished"
+        }
+    }
+
+    SaveTestSetVos(){
+        foreach ($testSetVo in $this.testSetVos) {
+            Write-Host "Creating Test Set with " $testSetVo.tests.Count " tests..." 
+            $testSetVo.create()
+            Write-Host "Created Test Set with " $testSetVo.tests.Count " tests" 
+        }
+    }
+
+    CreateTestVos(){
+        $this.testVos = @()
+        $testCaseNodes= $this.file.SelectNodes("/testsuite/testcase")
+        [int]$count = 0
+        foreach ($testCaseNode in $testCaseNodes) {
+            Write-Host "Iterating Node: " $testCaseNode.LocalName
+            $this.testVos = $this.testVos + $this.handleTestCaseNode($testCaseNode);
+        }
+        Write-Host "Tests Count: " +  $this.testVos.Count
+    }
+
+    SaveTestVos(){
+        foreach ($testVo in $this.testVos) {
+            $testVo.save()
+            $testVo.changeWorkflowStatus(11);
+        }
+    }
+
+    CreateTestPlanVo(){
+        $this.testPlanVo = [XrayTestPlanEntityVo]::new($this.testVos, $this.testSetVos)
+        
+    }
+
+    SaveTestPlanVo(){
+        $this.testPlanVo.create()
+    }
+
+    CreateTestExecutionVo(){
+        $this.testExecutionVo = [XrayTestExecutionEntityVo]::new($this.suiteName, $this.startDate, $this.endDate, $this.testPlanVo)
+    }
+
+    SaveTestExecutionVo(){
+        $this.testExecutionVo.create()
+    }
+    
+    [int]getTotalTestVos(){
+        $count = 0
+        $count = $this.testVos.Count;
+
+        foreach($testSet in $this.testSetVos){
+            $count = $count + $testSet.tests.Count
+        }
+        return $count
+    }
+
+
+    execute(){
+        $this.CreateTestVos()
+        #$this.CreateTestSetVos()
+        $this.SaveTestVos()
+        #$this.SaveTestSetVos()
+        $this.CreateTestPlanVo();
+        $this.SaveTestPlanVo()
+        $this.CreateTestExecutionVo();
+        $this.SaveTestExecutionVo()
+
+    }
+}
+
+$vo = [JUnitXmlProcessor]::new([Constants]::reportFilePath)
---
+++ script/XRayEntityVosDefinition.ps1
@@ -0,0 +1,359 @@
+﻿<# https://gallery.technet.microsoft.com/scriptcenter/ea40c1ef-c856-434b-b8fb-ebd7a76e8d91#content #>
+Import-Module 'C:\Users\bhansm\Documents\My Received Files\powershell\Get-IniContent.psm1'
+$ConfigFile = Get-IniContent 'C:\Users\bhansm\Desktop\JUnitXRayIntegration\config\config.ini'
+
+class XrayTestExecutionEntityVo{
+    [string]$name
+    [string]$startDate
+    [string]$endDate
+    [XrayTestPlanEntityVo]$testPlanVo
+    
+
+    XrayTestExecutionEntityVo($name, $startDate, $endDate, [XrayTestPlanEntityVo]$testPlanVo){
+        $this.name = $name
+        $this.startDate = $startDate
+        $this.endDate = $endDate
+        $this.testPlanVo = $testPlanVo
+    }
+
+    create(){
+        $url = [Constants]::url + "raven/1.0/import/execution"
+        $Headers = @{
+		    Authorization = [Credentials]::getEncodedValue()
+	    }
+
+        $tests = @()
+        foreach ($test in $this.testPlanVo.getTestVos()) {
+            $status = $test.getStatus()
+            $comment = $test.getComment()
+            #Write-Host $status
+            $obj = @{
+                "testKey" = $test.key;
+                "start" = $null;
+                "finish"= $null;
+                "comment" = $comment;
+                "status" = $status;
+            }
+            
+            $tests = $tests + $obj
+
+        }
+
+        $body = @{
+            "info" = @{
+            "summary" = $this.name + "(Created during importing junit result using REST at " + $(Get-Date).ToString([Constants]::currentDateFormat) + ")";
+            "description" = "This execution is automatically created when importing execution results from an external source";
+            #"version" = [Constants]::projectVersion;
+            "user" = "qtp";
+            #"revision" = "Revision Number";
+            #"startDate" = $this.startDate;
+            #"finishDate" = $this.endDate;
+            "testPlanKey" = $this.testPlanVo.key;
+            #"testEnvironments" = @("Env1", "Env2")
+          };
+          "tests" = $tests
+        }
+        Write-Host (ConvertTo-Json $body)
+
+        [Credentials]::setProtocols()
+        Write-Host "Creating Test Execution Plan"
+        $response = Invoke-WebRequest -Uri $url -Headers $Headers -Method Post -Body (ConvertTo-Json $body) -ContentType "application/json" 
+	    #$responseContent = ConvertFrom-Json ($response.Content)
+        Write-Host $response
+
+    }
+}
+
+
+class XrayTestPlanEntityVo{
+    [XrayTestEntityVo[]]$testVos
+    [XrayTestSetEntityVo[]]$testSetVos
+    [string]$key
+
+    XrayTestPlanEntityVo($testVos, $testSetVos){
+        $this.testVos = $testVos
+        $this.testSetVos = $testSetVos
+    }
+
+    [string[]] getKeys(){
+        [string[]]$keys = @()
+        foreach($vo in $this.testVos) {
+            $keys = $keys + $vo.key
+        }
+
+        foreach($vo in $this.testSetVos) {
+            $keys = $keys + $vo.getTestKeys()
+        }
+        return $keys
+    }
+
+    [XrayTestEntityVo[]] getTestVos(){
+        [XrayTestEntityVo[]]$vos = @()
+        foreach($vo in $this.testVos) {
+            $vos = $vos + $vo
+        }
+
+        foreach($vo in $this.testSetVos) {
+            $vos = $vos + $vo.getTestVos()
+        }
+        return $vos
+    }
+
+    create(){
+        $url = [Constants]::url + "api/2/issue"
+        $Headers = @{
+		    Authorization = [Credentials]::getEncodedValue()
+	    }
+
+        $body = @{
+            fields = @{
+                "project" = @{
+                    "key" = [Constants]::projectKey
+                };
+                "summary" = "Created during Junit-XRay Integration using REST at " + $(Get-Date).ToString([Constants]::currentDateFormat);
+                "description" = "Created during Junit-XRay Integration using REST at " + $(Get-Date).ToString([Constants]::currentDateFormat);
+                "issuetype" = @{
+                    "name" = "Test Plan"
+                 };
+                "customfield_10424" = $this.getKeys()
+             }
+         }
+         ConvertTo-Json $body
+        [Credentials]::setProtocols()
+        Write-Host "Creating Test Plan...."
+        $response = Invoke-WebRequest -Uri $url -Headers $Headers -Method Post -Body (ConvertTo-Json $body) -ContentType "application/json" 
+	    $responseContent = ConvertFrom-Json ($response.Content)
+        $this.key = $responseContent.key
+        Write-Host $response
+    }
+}
+
+class XrayTestSetEntityVo{
+    [XrayTestEntityVo[]]$tests
+    [string]$key
+
+    XrayTestSetEntityVo([XrayTestEntityVo[]]$tests){
+        $this.tests = $tests
+    }
+
+    [XrayTestEntityVo[]] getTestVos(){
+        return $this.tests
+    }
+
+    create(){
+        $url = [Constants]::url + "api/2/issue"
+        $Headers = @{
+		    Authorization = [Credentials]::getEncodedValue()
+	    }
+       
+        $testKey = @()
+
+        foreach ($test in $this.tests) {
+            $test.save()
+            $test.changeWorkflowStatus(11)
+            $testKey = $testKey + $test.key;
+        }
+
+        $body = @{
+            fields = @{
+                "project" = @{
+                    "key" = [Constants]::projectKey
+                };
+                "summary" = "Created during Junit-XRay Integration using REST at " + $(Get-Date).ToString([Constants]::currentDateFormat);
+                "description" = "Created during Junit-XRay Integration using REST at " + $(Get-Date).ToString([Constants]::currentDateFormat);
+                "issuetype" = @{
+                    "name" = "Test Set"
+                 };
+                "customfield_10410" = $testKey
+             }
+         }
+         ConvertTo-Json $body
+        [Credentials]::setProtocols()
+        Write-Host "Creating Test Set...."
+        $response = Invoke-WebRequest -Uri $url -Headers $Headers -Method Post -Body (ConvertTo-Json $body) -ContentType "application/json" 
+	    $responseContent = ConvertFrom-Json ($response.Content)
+        $this.key = $responseContent.key
+        Write-Host $response
+    }
+
+    [string[]] getTestKeys(){
+        $keys = @()
+        foreach($vo in $this.tests) {
+            $keys = $keys + $vo.key
+        }
+        return $keys
+    }
+}
+
+class XrayTestEntityVo
+{
+    [Fields]$fields;
+    [string]$key;
+    [string]$id;
+    [string]$self;
+    static [int]$count;
+    [string]$status;
+    <#[string]$start;
+    [string]$finish;#>
+    [string]$comment;
+      
+    XrayTestEntityVo([Fields]$fields){
+        $this.fields = $fields
+    }
+
+    static [XrayTestEntityVo] getInstance()
+    { 
+        return [XrayTestEntityVo]::new([Fields]::new([Project]::new([Constants]::projectKey), "summary for " + ++[XrayTestEntityVo]::count + " at " + $(get-date -f MM-dd-yyyy_HH_mm_ss), "desc for " + [XrayTestEntityVo]::count + " at " + $([Constants]::currentDate), [IssueType]::new("Test"), [TestType]::new("Generic"), "generic test definition"));
+    }
+
+    save(){
+        $url = [Constants]::url + "api/2/issue"
+        $Headers = @{
+		    Authorization = [Credentials]::getEncodedValue()
+	    }
+        $this.fields.summary = $this.fields.summary + " (Created during Junit-XRay Integration using REST at " + $(Get-Date).ToString([Constants]::currentDateFormat) + ")"
+        $this.fields.description = $this.fields.description + " (Created during Junit-XRay Integration using REST at " + $(Get-Date).ToString([Constants]::currentDateFormat) + ")"
+
+        [Credentials]::setProtocols()
+        Write-Host "Creating Test..."
+        $response = Invoke-WebRequest -Uri $url -Headers $Headers -Method Post -Body (ConvertTo-Json $this) -ContentType "application/json" 
+	    $responseContent = ConvertFrom-Json ($response.Content)
+        $this.id = $responseContent.id
+        $this.key = $responseContent.key
+        $this.self = $responseContent.self
+        Write-Host $response
+    }
+
+    [string]getComment(){
+        return $this.comment;
+    }
+
+    setComment([string]$comment){
+        $this.comment = $comment
+    }
+
+    setStatus([string]$status){
+        $this.status = $status
+    }
+
+    [string]getStatus(){
+        If ($this.status -eq 'Success')  {
+            return 'PASS'
+        } ElseIf($this.status -eq 'Ignored') {
+            return 'TODO'
+        } ElseIf($this.status -eq 'Failed') {
+            return 'FAIL'
+        }
+        return $this.status
+    }
+
+    changeWorkflowStatus([int]$transitionId){
+        $url = [Constants]::url + "api/2/issue/$($this.key)/transitions?expand=transitions.fields"
+        $Headers = @{
+		    Authorization = [Credentials]::getEncodedValue()
+	    }
+        $body = @{
+            transition = @{
+                id = $transitionId
+            }
+        }
+        [Credentials]::setProtocols()
+        Write-Host "Test Transition" 
+        $response = Invoke-WebRequest -Uri $url -Headers $Headers -Method Post -Body (ConvertTo-Json $body) -ContentType "application/json" 
+	    #$responseContent = ConvertFrom-Json ($response.Content)
+        Write-Host $response
+    }
+}
+
+class Fields{
+    [Project]$project;
+    [string]$summary;
+    [string]$description;
+    [IssueType]$issuetype;
+    [TestType]$customfield_10400;
+    [string]$customfield_10403; 
+
+    Fields(){
+    }
+
+    Fields([Project]$project, [string]$summary, [string]$description, [IssueType]$issuetype, [TestType]$customfield_10400, [string]$customfield_10403){
+        $this.project = $project
+        $this.summary = $summary
+        $this.description = $description
+        $this.issuetype = $issuetype
+        $this.customfield_10400 = $customfield_10400
+        $this.customfield_10403 = $customfield_10403
+    }
+}
+
+class TestType{
+    [string]$value;
+    
+    TestType([string]$value){
+        $this.value = $value;
+    }
+}
+
+class IssueType{
+    [string]$name;
+
+    IssueType([string]$name){
+        $this.name = $name;
+    }
+}
+
+class Project{
+    [string]$key;
+
+    Project([string]$key){
+        $this.key = $key;
+    }
+}
+
+class Constants{
+    
+    static [string]$url = $ConfigFile["server"]["url"] + "/rest/";
+    static [string]$projectKey = $ConfigFile["project"]["key"];
+    <#static [string]$projectVersion = $ConfigFile["project"]["version"];#>
+    static [string]$reportFilePath = $ConfigFile["report"]["filepath"];
+    static [string]$currentDateFormat = "dd-MMM-yyyy HH:mm:ss";
+    
+    static Reload(){
+        [Constants]::url = $global:ConfigFile["server"]["url"] + "/rest/";
+        [Constants]::projectKey = $global:ConfigFile["project"]["key"];
+        #[Constants]::projectVersion = $global:ConfigFile["project"]["version"];
+        [Constants]::reportFilePath = $global:ConfigFile["report"]["filepath"];
+        
+    }
+}
+
+[Constants]::Reload()
+
+class Credentials{
+    static [string]$user = $ConfigFile["credentials"]["user"];
+    static [string]$password = $ConfigFile["credentials"]["password"];
+    
+    static [string] getEncodedValue(){
+        $pair = "$([Credentials]::user):$([Credentials]::password)"
+	    $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($pair))
+	    return "Basic $encodedCreds"
+    }
+
+    static setProtocols(){
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy1 : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy1
+    }
+
+}
