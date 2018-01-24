bf9b4548a25e7c390a8ae3e5e9dbf905b361947b
Bhansali, Mithul <mithul.bhansali@dieboldnixdorf.com>
2018-01-24 13:04:13 +0530

    2C - Version 2 - Multiple Digits Consideration

--- utility/Utility.ps1
+++ utility/Utility.ps1
@@ -1,19 +1,44 @@
-﻿class UtilityProcessor{
-    [System.Xml.XmlDocument]$file
+﻿Function Convert-Content {  
+    [CmdletBinding()]  
+    Param(  
+        [ValidateNotNullOrEmpty()]  
+        [string]$InputFilePath,
+        [ValidateNotNullOrEmpty()]  
+        [string]$OutputFilePath  
+    )  
+      
+    Begin  
+        {Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started"}  
+          
+    Process  
+    {  
+        Write-Verbose "$($MyInvocation.MyCommand.Name):: Processing file: $Filepath"  
+              
+        
+        [UtilityProcessor]::new($InputFilePath, $OutputFilePath)
+    }  
+          
+    End  
+        {Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended"}  
+}
+
+
+class UtilityProcessor{
+    [System.Xml.XmlDocument]$xmlDoc
     [string]$outputFilePath
     
-    UtilityProcessor($filePath, $outputFilePath){
-        [System.Xml.XmlDocument]$this.file = new-object System.Xml.XmlDocument
+    UtilityProcessor($inputFilePath, $outputFilePath){
+        [System.Xml.XmlDocument]$this.xmlDoc = new-object System.Xml.XmlDocument
         $this.outputFilePath = $outputFilePath
-        $this.file.load($filePath)
+        $this.xmlDoc.load($inputFilePath)
         $this.init()
     }
 
     init(){
-        $dayNodes = $this.file.SelectNodes('//Day')
+        $dayNodes = $this.xmlDoc.SelectNodes('//Day')
         foreach($dayNode in $dayNodes){
             $text = $dayNode.InnerText
-            $m = $text -match "(\w*)\s*([-+]?)\s*(\d?)"
+            $m = $text -match "(CURRENT)([-+])(\d*)"
             if($m){
                 $monthNode = $dayNode.PreviousSibling
                 $yearNode = $monthNode.PreviousSibling
@@ -30,21 +55,17 @@
                 
                     #doc.CreateCDataSection("Hi, How are you..??")
                     #$dayNode.AppendChild($this.file.CreateCDataSection($dayText))
-                    $dayNode.AppendChild($this.file.CreateTextNode($dayText))
-                    $monthNode.AppendChild($this.file.CreateTextNode($monthText))
-                    $yearNode.AppendChild($this.file.CreateTextNode($yearText))
+                    $dayNode.AppendChild($this.xmlDoc.CreateTextNode($dayText))
+                    $monthNode.AppendChild($this.xmlDoc.CreateTextNode($monthText))
+                    $yearNode.AppendChild($this.xmlDoc.CreateTextNode($yearText))
                 }
-            } else {
-                Write-Host $m
             }
             
         }
-        $this.file.save($this.outputFilePath)
-        (Get-Content $this.outputFilePath).replace('&lt;', '<') | Set-Content $this.outputFilePath
-        (Get-Content $this.outputFilePath).replace('&gt;', '>') | Set-Content $this.outputFilePath
+        $this.xmlDoc.save($this.outputFilePath)
+        (Get-Content $this.outputFilePath).replace('&lt;', '<').replace('&gt;', '>') | Set-Content $this.outputFilePath
+        #(Get-Content $this.outputFilePath).replace('&gt;', '>') | Set-Content $this.outputFilePath
         
     }
 
 }
-
-[UtilityProcessor]::new('C:\Users\bhansm\Downloads\ATM04ATM04.xml', "C:\Users\bhansm\Desktop\JUnitXRayIntegration\utility\file.xml")
