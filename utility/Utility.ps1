0d4f2d2250a5e3da7f41cda419974f4897ddd1d5
Bhansali, Mithul <mithul.bhansali@dieboldnixdorf.com>
2018-01-24 16:30:58 +0530

    5C - Release Version 2. One directory Pprocessing instead of one File

--- utility/Utility.ps1
+++ utility/Utility.ps1
@@ -4,7 +4,9 @@
         [ValidateNotNullOrEmpty()]  
         [string]$InputFilePath,
         [ValidateNotNullOrEmpty()]  
-        [string]$OutputFilePath  
+        [string]$OutputFilePath,
+        [ValidateNotNullOrEmpty()]  
+        [string]$InputDirPath  
     )  
       
     Begin  
@@ -14,8 +16,21 @@
     {  
         Write-Verbose "$($MyInvocation.MyCommand.Name):: Processing file: $Filepath"  
               
+        $files = Get-ChildItem $InputDirPath | Where-Object {$_.Name -match "(?=.*.xml.LOG$)"}
+        $OutputDirPath = $($InputDirPath + [System.IO.Path]::DirectorySeparatorChar + "output")
+        if(-Not (Test-Path -Path $OutputDirPath)) {
+            New-Item -ItemType Directory -Force -Path $OutputDirPath
+        }
+
+        foreach($file in $files){
+            $fileName = $file.Name
+            $InputFilePath = $file.FullName
+            $OutputFilePath = $($OutputDirPath + [System.IO.Path]::DirectorySeparatorChar + $fileName) 
+            Write-Host "Processing " $($InputFilePath) " ..."
+            $vo = [UtilityProcessor]::new($InputFilePath, $OutputFilePath)
+            $vo.execute()
+        }
         
-        [UtilityProcessor]::new($InputFilePath, $OutputFilePath)
     }  
           
     End  
@@ -31,6 +46,9 @@ class UtilityProcessor{
         [System.Xml.XmlDocument]$this.xmlDoc = new-object System.Xml.XmlDocument
         $this.outputFilePath = $outputFilePath
         $this.xmlDoc.load($inputFilePath)
+    }
+
+    execute(){
         $this.init()
     }
 
@@ -40,8 +58,8 @@ class UtilityProcessor{
             $text = $dayNode.InnerText
             $m = $text -match "(CURRENT)([-+])(\d*)"
             if($m){
-                $monthNode = $this.getSibling($dayNode, 'Month', 'Element')
-                $yearNode = $this.getSibling($dayNode, 'Year', 'Element')
+                $monthNode = $this.getSiblingNode($dayNode, 'Month', 'Element')
+                $yearNode = $this.getSiblingNode($dayNode, 'Year', 'Element')
                 
                 if($monthNode -ne $null -and $yearNode -ne $null){
                     $dayNode.set_innerXML($null)
@@ -53,31 +71,38 @@ class UtilityProcessor{
                     $monthText = "<CURRDATE" + $($plusMinusText) + $($numberText) + ",mm>"
                     $dayText = "<CURRDATE" + $($plusMinusText) + $($numberText) + ",dd>"
                 
-                    #doc.CreateCDataSection("Hi, How are you..??")
-                    #$dayNode.AppendChild($this.file.CreateCDataSection($dayText))
                     $dayNode.AppendChild($this.xmlDoc.CreateTextNode($dayText))
                     $monthNode.AppendChild($this.xmlDoc.CreateTextNode($monthText))
                     $yearNode.AppendChild($this.xmlDoc.CreateTextNode($yearText))
                 }
             }
-            
         }
         $this.xmlDoc.save($this.outputFilePath)
         (Get-Content $this.outputFilePath).replace('&lt;', '<').replace('&gt;', '>') | Set-Content $this.outputFilePath
-        #(Get-Content $this.outputFilePath).replace('&gt;', '>') | Set-Content $this.outputFilePath
         
     }
 
 
-    [object] getSibling($node, $siblingNodeName, $siblingNodeType){
+    [object] getSiblingNode($node, $siblingNodeName, $siblingNodeType){
         $parentNode = $node.ParentNode;
         $siblingNodes = $parentNode.ChildNodes
+        $precedingSiblingNodes = $node.SelectNodes("./preceding-sibling::" + $siblingNodeName);
 
-        foreach($siblingNode in $siblingNodes){
-            if($siblingNode.NodeType -eq $siblingNodeType -and $siblingNode.LocalName -eq $siblingNodeName){
-                return $siblingNode;
+        $siblingNode = $this.getNode($node.SelectNodes("./preceding-sibling::" + $siblingNodeName),$siblingNodeName, $siblingNodeType)
+
+        if($siblingNode -ne $null) {
+            return $siblingNode
+        }
+        return $this.getNode($node.SelectNodes("./following-sibling::" + $siblingNodeName),$siblingNodeName, $siblingNodeType);
+    } 
+
+    [object] getNode($nodes, $nodeName, $nodeType){
+        foreach($node in $nodes){
+            if($node.NodeType -eq $nodeType -and $node.LocalName -eq $nodeName){
+                return $node;
             }
         }
         return $null;
-    } 
+    }
+
 }
